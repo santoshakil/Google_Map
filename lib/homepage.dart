152113591a52ec033550fad730b420e2e07c775f
Kakon007 <kakon443@gmail.com>
2020-12-06 17:49:04 +0600

    2nd Commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -9,7 +10,9 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
-  Completer<GoogleMapController> _controller = Completer();
+  GoogleMapController _controller ;
+  String searchaddress;
+
 
   @override
   void initState() {
@@ -20,65 +23,68 @@ class HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black,
-        leading: IconButton(
-            icon: Icon(FontAwesomeIcons.arrowLeft),
-            onPressed: () {
-              //
-            }),
-        title: Text("Dhaka"),
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(FontAwesomeIcons.search),
-              onPressed: () {
-                //
-              }),
-        ],
+        backgroundColor: Colors.yellow,
+        // leading: IconButton(
+        //     icon: Icon(FontAwesomeIcons.arrowLeft),
+        //     onPressed: () {
+        //       //
+        //     }),
+        title: Text("Search Location",style: TextStyle(color: Colors.white),),
+        // actions: <Widget>[
+        //   IconButton(
+        //       icon: Icon(FontAwesomeIcons.search),
+        //       onPressed: () {
+        //         //
+        //       }),
+        // ],
       ),
-      body: Stack(
-        children: <Widget>[
-          _buildGoogleMap(context),
-          // _zoomminusfunction(),
-          // _zoomplusfunction(),
-          _buildContainer(),
-        ],
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            _buildGoogleMap(context),
+            // _zoomminusfunction(),
+            // _zoomplusfunction(),
+            _searchlocation(context),
+            _buildContainer(),
+          ],
+        ),
       ),
     );
   }
+  //
+  // Widget _zoomminusfunction() {
+  //
+  //   return Align(
+  //     alignment: Alignment.topLeft,
+  //     child: IconButton(
+  //         icon: Icon(FontAwesomeIcons.searchMinus,color:Color(0xff6200ee)),
+  //         onPressed: () {
+  //           zoomVal--;
+  //           _minus( zoomVal);
+  //         }),
+  //   );
+  // }
+  // Widget _zoomplusfunction() {
+  //
+  //   return Align(
+  //     alignment: Alignment.topRight,
+  //     child: IconButton(
+  //         icon: Icon(FontAwesomeIcons.searchPlus,color:Color(0xff6200ee)),
+  //         onPressed: () {
+  //           zoomVal++;
+  //           _plus(zoomVal);
+  //         }),
+  //   );
+  // }
 
-  Widget _zoomminusfunction() {
-
-    return Align(
-      alignment: Alignment.topLeft,
-      child: IconButton(
-          icon: Icon(FontAwesomeIcons.searchMinus,color:Color(0xff6200ee)),
-          onPressed: () {
-            zoomVal--;
-            _minus( zoomVal);
-          }),
-    );
-  }
-  Widget _zoomplusfunction() {
-
-    return Align(
-      alignment: Alignment.topRight,
-      child: IconButton(
-          icon: Icon(FontAwesomeIcons.searchPlus,color:Color(0xff6200ee)),
-          onPressed: () {
-            zoomVal++;
-            _plus(zoomVal);
-          }),
-    );
-  }
-
-  Future<void> _minus(double zoomVal) async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(40.712776, -74.005974), zoom: zoomVal)));
-  }
-  Future<void> _plus(double zoomVal) async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(40.712776, -74.005974), zoom: zoomVal)));
-  }
+  // Future<void> _minus(double zoomVal) async {
+  //   final GoogleMapController controller = await _controller.future;
+  //   controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(40.712776, -74.005974), zoom: zoomVal)));
+  // }
+  // Future<void> _plus(double zoomVal) async {
+  //   final GoogleMapController controller = await _controller.future;
+  //   controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(40.712776, -74.005974), zoom: zoomVal)));
+  // }
 
 
   Widget _buildContainer() {
@@ -258,7 +264,9 @@ class HomePageState extends State<HomePage> {
         mapType: MapType.normal,
         initialCameraPosition:  CameraPosition(target: LatLng(23.810331, 90.412521), zoom: 12),
         onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
+          setState(() {
+            _controller=controller;
+          });
         },
         markers: {
           // newyork1Marker,newyork2Marker,newyork3Marker,
@@ -269,9 +277,49 @@ class HomePageState extends State<HomePage> {
     );
   }
 
+  Widget _searchlocation(BuildContext context) {
+    return Positioned(
+      top: 30,
+      left: 15,
+      right: 15,
+      child: Container(
+        height: 50,
+        width: double.infinity,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            color: Colors.white
+        ),
+        child: TextField(
+          decoration: InputDecoration(
+              hintText: "Enter Address",
+              alignLabelWithHint: true,
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top:15,left: 15,right: 15),
+              suffixIcon: IconButton(icon: Icon(Icons.search) ,onPressed: searchaddressandNav,iconSize: 30,)
+          ),
+          onChanged: (value){
+            setState(() {
+              searchaddress=value;
+            });
+          },
+        ),
+
+      ),
+    );
+  }
+
+  searchaddressandNav(){
+    Geolocator().placemarkFromAddress(searchaddress).then((result) {
+      _controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+        target:
+        LatLng(result[0].position.latitude, result[0].position.longitude),
+        zoom: 10.0)));
+  });
+  }
+
   Future<void> _gotoLocation(double lat,double long) async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(lat, long), zoom: 15,tilt: 50.0,
+    final GoogleMapController controller = await _controller;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(lat, long), zoom: 16,tilt: 50.0,
       bearing: 45.0,)));
   }
 }
